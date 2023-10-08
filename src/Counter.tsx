088ea2e75f4ee542635785974e1510fd191aea8a
Konstantin <kapurelloruru@gmail.com>
2023-10-08 22:50:52 +0200

    Added preloadedState with Localstorage

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,12 +2,17 @@ import React, {useState} from 'react';
 import './Counter.css';
 import Indicator from "./components/Indicator/Indicator";
 import Setter from './components/Setter/Setter';
+import {useSelector} from "react-redux";
+import {RootStateType} from "./redux/redux-store";
 
 
 const Counter = () => {
 
-  const [inputMinValue, setInputMinValue] = useState<number>(0)
-  const [inputMaxValue, setInputMaxValue] = useState<number>(5)
+  const settedMinValue = useSelector<RootStateType, number>(state => state.counter.minValue)
+  const settedMaxValue = useSelector<RootStateType, number>(state => state.counter.maxValue)
+
+  const [inputMinValue, setInputMinValue] = useState<number>(settedMinValue)
+  const [inputMaxValue, setInputMaxValue] = useState<number>(settedMaxValue)
 
   const isCorrectRange = inputMinValue < inputMaxValue
     && inputMinValue >= 0
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,8 +4,27 @@ import ReducerCounter from "./reducers/reducerCounter";
 const rootReducer = combineReducers({
   counter: ReducerCounter
 })
+const savedState = localStorage.getItem('app-state')
+
+let preloadedState
+
+if (savedState) {
+  try {
+    preloadedState = JSON.parse(savedState)
+  } catch (e) {
+    console.log('Ошибонька: ', e)
+  }
+}
+
+
 
 export type RootStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, preloadedState)
 
+
+
+store.subscribe(() => {
+  const currentState = store.getState()
+  localStorage.setItem('app-state', JSON.stringify(currentState))
+})
